ec402a36c333b71788c9d50dec33efb47a1ef796
praneshnikhar <praneshnikhar@gmail.com>
2025-08-10 11:11:14 +0530

    hmm

---
+++ rustComplete/src/stack.rs
@@ -0,0 +1,12 @@
+fn main(){
+    let x = 42;
+    let y = 10;
+    let z = add_numbers(x,y);
+    println!("the result is {}",z);
+}
+
+fn add_numbers(a:i32, b:i32) -> i32{
+    let c= a+b;
+    c
+
+}
