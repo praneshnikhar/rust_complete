24683bd1ef726b86eba5ae9db49b942b49092e99
praneshnikhar <praneshnikhar@gmail.com>
2025-08-12 23:32:47 +0530

    hmm

---
+++ rustComplete/src/borrowing.rs
@@ -0,0 +1,18 @@
+fn main(){
+    let mut s = String :: from("hello");
+    let r1 = &mut s;            //this would violate the first rule of borrowing , which says that
+    let r2 = &mut s;            //we can only have one mutable refrence to the same data at a time
+
+    println!("{}, {}", r1, r2);
+}
+
+
+//THIS WE CAN DO 
+fn main(){
+let mut s = String :: from ("Helllo");
+{
+    let r1 = &mut s;
+}//r1 goes out of scope here , so we can make a new reference with 
+
+let r2 = &mut s;
+}
