09bded1656b14c097efc920553b2897ac4608423
praneshnikhar <praneshnikhar@gmail.com>
2025-08-22 11:10:56 +0530

    str

--- rustComplete/src/strSlice.rs
+++ rustComplete/src/strSlice.rs
@@ -15,11 +15,30 @@
 // }
 
 
-fn main(){
-    let mut s :String = String :: from ("hello");
-    s.push_str("hello, world");
-    s.push('!');
+// fn main(){
+//     let mut s :String = String :: from ("hello");
+//     s.push(',');
+//     s.push_str("world");
+//     s+= "!";
 
-    assert_eq!(s, "hello , world!");
-    println!("Success!");
+//     println!("{}", s); 
+// }
+
+
+
+// fn main(){
+//     let s:String= String::from("i like dogs");
+//     let s1 = s.replace("dogs", "cats");
+
+//     assert_eq!(s1, "i like catds");
+//     println!("Success");
+// }
+
+
+fn main(){
+    let s1:String = String::from("hello, ");
+    let s2:String = String::from("world!");
+    let s3:String = s1+s2.as_str();
+    assert_eq!(s3, "hello , wolrd!");
+    println!("{}", s3);
 }
